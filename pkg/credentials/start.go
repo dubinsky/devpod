cd282d0bd0f997e8b7689e745ea6f5a26272a569
Samuel K <skevetter@pm.me>
2026-02-03 12:55:12 -0600

    refactor(cmd/agent): dockerless build logic to pkg/agent (#426)
    Signed-off-by: Samuel K <skevetter@pm.me>

--- pkg/credentials/start.go
+++ pkg/credentials/start.go
@@ -15,45 +15,46 @@ import (
 	"github.com/skevetter/log"
 )
 
-func StartCredentialsServer(ctx context.Context, cancel context.CancelFunc, client tunnel.TunnelClient, log log.Logger) (int, error) {
+func StartCredentialsServer(ctx context.Context, client tunnel.TunnelClient, log log.Logger) (int, error) {
 	port, err := portpkg.FindAvailablePort(random.InRange(13000, 17000))
 	if err != nil {
 		return 0, err
 	}
 
 	go func() {
-		defer cancel()
-
 		err := RunCredentialsServer(ctx, port, client, log)
 		if err != nil {
 			log.WithFields(logrus.Fields{"error": err}).Error("error running git credentials server")
 		}
 	}()
 
-	// wait until credentials server is up
+	if err := waitForServer(ctx, port, log); err != nil {
+		return 0, err
+	}
+	return port, nil
+}
+
+func waitForServer(ctx context.Context, port int, log log.Logger) error {
 	maxWait := time.Second * 4
 	now := time.Now()
-Outer:
 	for {
 		err := PingURL(ctx, "http://localhost:"+strconv.Itoa(port))
 		if err != nil {
 			select {
 			case <-ctx.Done():
-				break Outer
+				return ctx.Err()
 			case <-time.After(time.Second):
 			}
 		} else {
 			log.Debug("credentials server started")
-			break
+			return nil
 		}
 
 		if time.Since(now) > maxWait {
 			log.Debug("credentials server did not start in time")
-			break
+			return fmt.Errorf("credentials server did not start in time")
 		}
 	}
-
-	return port, nil
 }
 
 func PingURL(ctx context.Context, url string) error {
