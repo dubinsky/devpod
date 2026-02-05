e1720404e41fd6de42e80a8fbf969394dfff858b
Samuel K <skevetter@pm.me>
2026-02-05 17:28:01 -0600

    refactor(ssh): options to struct and handle ssh exit codes (#447)
    Signed-off-by: Samuel K <skevetter@pm.me>

--- pkg/devcontainer/sshtunnel/sshtunnel.go
+++ pkg/devcontainer/sshtunnel/sshtunnel.go
@@ -158,8 +158,14 @@ func ExecuteCommand(
 		defer func() { _ = streamer.Close() }()
 
 		log.WithFields(logrus.Fields{"command": command}).Debug("running agent command in SSH tunnel")
-		err = devssh.Run(ctx, sshClient, command, gRPCConnStdinReader, gRPCConnStdoutWriter, streamer, nil)
-		if err != nil {
+		if err := devssh.Run(devssh.RunOptions{
+			Context: ctx,
+			Client:  sshClient,
+			Command: command,
+			Stdin:   gRPCConnStdinReader,
+			Stdout:  gRPCConnStdoutWriter,
+			Stderr:  streamer,
+		}); err != nil {
 			_ = streamer.Close()
 			if out := streamer.ErrorOutput(); out != "" {
 				errChan <- fmt.Errorf("run agent command failed: %w\n%s", err, out)
